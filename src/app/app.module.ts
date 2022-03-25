4b7a74c954393dabeadcba7c8ebb2ea0328efe1b
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-03-25 20:42:49 -0300

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';]
+import { HelloComponent } from './hello.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TodoListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
