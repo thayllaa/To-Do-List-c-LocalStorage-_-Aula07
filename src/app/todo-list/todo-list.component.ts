4368abee6b255f72d2e3c9f57d3c48bd3177d0bd
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-03-25 20:46:31 -0300

    1 file modified

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,3 +1,4 @@
+import { newArray } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+
+  
+  userArrays = newArray();
 
   ngOnInit() {
   }
